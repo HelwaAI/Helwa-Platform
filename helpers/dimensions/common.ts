06c629881f4ba0be1a43b2f40f832e15f1a95909
CCosmoe <taliyah052@gmial.com>
2025-11-24 23:04:02 -0600

    zones_WORKING

---
+++ helpers/dimensions/common.ts
@@ -0,0 +1,9 @@
+/**
+ * Represents a position and length in bitmap coordinates.
+ */
+export interface BitmapPositionLength {
+  /** The position (x or y coordinate) in bitmap pixels */
+  position: number;
+  /** The length (width or height) in bitmap pixels */
+  length: number;
+}
