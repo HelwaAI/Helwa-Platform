ab72460b0ab444ee5a2fb66d4e6f063946c24639
CCosmoe <taliyah052@gmial.com>
2025-11-28 23:41:28 -0600

    increasezone5mtime

--- app/api/crypto/zones/route.ts
+++ app/api/crypto/zones/route.ts
@@ -51,7 +51,7 @@ export async function GET(request: Request) {
       ORDER BY s.symbol, z.bottom_price DESC
       LIMIT $2`;
               
-    
+    // Old Query
     // const query = `
     //   SELECT
     //     s.symbol,
